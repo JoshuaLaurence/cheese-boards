1ba89b07116ac6236a2ddbe1730d8a14b5fb5594
Joshua Laurence <laurence11@fb.com>
2022-11-03 14:55:43 +0000

    Began population seed function with data, starting with test users

--- db/seed.js
+++ db/seed.js
@@ -3,5 +3,26 @@ const db = require("../db/db")
 
 async function seed() {
 
+    await db.sync({force:true})
+
+    await User.bulkCreate([
+        {
+            name: "Vincent",
+            email: "vince@dreamland.com"
+        },
+        {
+            name: "Billy Summers",
+            email: "b.summers@alice.com"
+        },
+        {
+            name: "Bob The Builder",
+            email: "bob@thebuilder.com"
+        },
+        {
+            name: "Mr Reade",
+            email: "jack.reade@thewell.co.uk"
+        }
+    ])
+
 }
 seed();
--- models/index.js
+++ models/index.js
@@ -2,4 +2,15 @@ const Board = require("./board.model");
 const Cheese = require("./cheese.model");
 const User = require("./user.model");
 
+//Associations
+
+//ONE User can have MANY Boards
+User.hasMany(Board);
+Board.belongsTo(User);
+
+//MANY Cheeses can belong to MANY Boards
+Board.belongsToMany(Cheese, {through: "Board_Cheese"})
+Cheese.belongsToMany(Board, {through: "Board_Cheese"})
+
+
 module.exports = { Board, Cheese, User }
