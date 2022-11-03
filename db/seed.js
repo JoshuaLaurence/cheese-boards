b2ec6e7773b568c2ee7f6bea9ed3caa41c24d481
Joshua Laurence <laurence11@fb.com>
2022-11-03 15:15:04 +0000

    Added the cheese boards to the seed function. Got creative with the descriptions

--- db/seed.js
+++ db/seed.js
@@ -24,5 +24,37 @@ async function seed() {
         }
     ])
 
+    await Board.bulkCreate([
+        {
+            type: "The Blue Cheese Board",
+            description: "Served on a thick slap of veneered walnut, this pungent board will have your taste buds sizzling. Gorgonzola, Blue Stilton, and a couple more fan favourites",
+            rating: 8.4
+        },
+        {
+            type: "The American Cheese Board",
+            description: "It's bad for you on so many levels, hence it remains a fan favourite. Ten slices of still individually wrapped american cheese served on a thin yet weighty slab of concrete",
+            rating: 9.2
+        },
+        {
+            type: "The British Cheese Board",
+            description: "Blue Stilton, Double Gloucester, and a dense wedge of Wensleydale meet in the confines of a small wicker basket. A cup of tea is an optional and favourable extra",
+            rating: 8.1
+        },
+        {
+            type: "The Soft Cheese Board",
+            description: "Brie, Camembert, Feta and Ricotta all meet on a rounded and polished circular platter hand crafted from red oak",
+            rating: 6.8
+        },
+        {
+            type: "The Semi-Hard Cheese Board",
+            description: "Gouda, Swiss and a healthy dose of Monterey Jack. Displayed on a solid subsection of marble, a safe and mouthwatering bet",
+            rating: 7.9
+        },
+        {
+            type: "The Fresh Cheese Board",
+            description: "An unusual and yet delicious choice. Mozeralla, Feta, Brie and swiss cheese all served on a natural slice of slate",
+            rating: 7.3
+        },
+    ])
 }
 seed();
